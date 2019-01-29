b3791668de2f86821767ca525184521fcbfefbf4
Jordan <dumlaoj@hawaii.edu>
2019-01-28 16:22:19 -0800

    Initial Commit

---
+++ Ripple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ripple
+//
+//  Created by Jordan Dumlao on 1/28/19.
+//  Copyright © 2019 Jordan Dumlao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
